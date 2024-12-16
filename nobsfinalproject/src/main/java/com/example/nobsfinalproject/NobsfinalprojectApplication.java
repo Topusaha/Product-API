eacfb5a18b236e5fe524e2bb3811adf02f31667f
Topu Saha <topusaha@Topus-Air.lan>
2024-12-16 15:10:02 -0500

    Inital Commit

---
+++ nobsfinalproject/src/main/java/com/example/nobsfinalproject/NobsfinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.nobsfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobsfinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobsfinalprojectApplication.class, args);
+	}
+
+}
---
+++ nobsfinalproject/src/test/java/com/example/nobsfinalproject/NobsfinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nobsfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobsfinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
